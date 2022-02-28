0e952d0a75a6f39170ea72ecccc4e181098c6b5c
erka <erdenebold10@gmail.com>
2022-02-28 11:04:16 +0800

    add abico shell

---
+++ remote_code_update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/expect -f
+spawn ssh abico@203.91.116.148
+expect "Password:*"
+send "serverpass\r"
+expect "$ "
+interact
+
+cd /opt/odoo13/abico_erp
+sudo git pull
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 start
