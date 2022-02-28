66da1832c4264233413bf4fc8126d345a31c6fc1
erka <erdenebold10@gmail.com>
2022-02-28 10:02:04 +0800

    add abico shell

--- abico_code_update.sh
+++ abico_code_update.sh
@@ -1,10 +1,10 @@
 cd /opt/odoo13/abico_erp
 sudo git pull
-sudo /etc/init.d/odoo-server stop
-sudo /etc/init.d/odoo-server stop
-sudo /etc/init.d/odoo-server stop
-sudo /etc/init.d/odoo-server stop
-sudo /etc/init.d/odoo-server stop
-sudo /etc/init.d/odoo-server stop
-sudo /etc/init.d/odoo-server stop
-sudo /etc/init.d/odoo-server start
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 stop
+sudo /etc/init.d/odoo-server13 start
